f8fef6179166eeecdc08d70ae64e278c50a02781
Noah Lee <lee.noah@outlook.com>
2025-08-11 22:34:23 -0400

    Update api cors config to add PATCH

--- api/src/cors/config.ts
+++ api/src/cors/config.ts
@@ -1,7 +1,5 @@
-console.log("process.env.CORS_ORIGIN", process.env.CORS_ORIGIN);
-
 export const CORS_CONFIG = {
   origin: process.env.CORS_ORIGIN!,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
