6d08b796000f27019f4cd5d69c80b0c907fcc1e5
Noah Lee <lee.noah@outlook.com>
2025-08-14 11:11:38 -0400

    Add db time testing endpoint

--- api/src/api/routes.ts
+++ api/src/api/routes.ts
@@ -1,12 +1,17 @@
 import { FastifyInstance } from "fastify";
 import nutritionRoutes from "@/api/nutrition/routes";
 import authRoutes from "@/api/auth/routes";
-import { handleAuth } from "@/api/auth/handlers";
+import db from "@/db/config";
+import { sql } from "kysely";
 
 const apiRoutes = (fastify: FastifyInstance) => {
+  fastify.get("/time", async (_, reply) => {
+    const res = await db.executeQuery(sql`SELECT NOW() AS now`.compile(db));
+    return reply.status(200).send(res);
+  });
+
   fastify.register(nutritionRoutes, {
     prefix: "/nutrition",
-    preHandler: handleAuth,
   });
   fastify.register(authRoutes, { prefix: "/auth" });
 };
