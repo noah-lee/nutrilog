05aafad8000e16fb03417fd410d10214f5e6b939
Noah Lee <lee.noah@outlook.com>
2025-08-24 20:14:14 -0400

    Change api port to 8000

--- api/src/index.ts
+++ api/src/index.ts
@@ -28,7 +28,7 @@ fastify.register(apiRoutes, { prefix: "/api" });
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    await fastify.listen({ port: 8000, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
